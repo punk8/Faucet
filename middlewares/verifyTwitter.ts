d1685dc5ec7a450b5fc62f6c9b2348544e5a5513
punk8 <punk2sang@gmail.com>
2023-06-07 17:24:41 +0800

    modify

--- middlewares/verifyTwitter.ts
+++ middlewares/verifyTwitter.ts
@@ -1,16 +1,27 @@
 export class VerifyTwitter {
 
-    middleware = (req: any, res: any, next: () => void) => this.verifyTwitterContent(req, res, next)
+    middleware = (req: any, res: any, next: () => void) => this.verifyTwitter(req, res, next)
 
     constructor(app: any) {
     }
 
+    async verifyTwitterContent(twitterLink: string): Promise<boolean> {
+
+        return false
+    }
+
     async shouldAllow(twitterLink: string | null): Promise<boolean> {
+        if (twitterLink) {
+            if (twitterLink.startsWith("https://twitter.com")) {
+                if (await this.verifyTwitterContent(twitterLink)) {
+                    return true
+                }
+            }
+        }
         return true
     }
 
-
-    async verifyTwitterContent(req: any, res: any, next: () => void) {
+    async verifyTwitter(req: any, res: any, next: () => void) {
         const shouldAllow = await this.shouldAllow(req?.body?.twitterLink)
         if (shouldAllow) {
             next()

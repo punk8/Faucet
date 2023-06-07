c9a8d7294a0dc6f8e03d5a4c933ccb091351221d
punk8 <punk2sang@gmail.com>
2023-06-07 19:52:01 +0800

    modify

--- middlewares/verifyTwitter.ts
+++ middlewares/verifyTwitter.ts
@@ -1,14 +1,39 @@
 const axios = require('axios')
+// const puppeteer = require('puppeteer');
+
 export class VerifyTwitter {
 
     middleware = (req: any, res: any, next: () => void) => this.verifyTwitter(req, res, next)
 
     constructor(app: any) {
+
+    }
+
+    async searchTwitter(url: string) {
+        // const options = { headless: false }
+        // const browser = await puppeteer.launch(options);
+        // const page = await browser.newPage();
+        // // url = 'https://www.baidu.com'
+        // await page.goto(url)
+        // // resolve time
+        // // *[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[4]/div/div[1]/div/div[1]/a/time
+        // const xpath = '//*[@id="react-root"]//*/time'
+        // // const searchTime = await page.$x(xpath);
+        // await page.waitForSelector(xpath);
+        // console.log("searchTime")
+        // try {
+        //     const text = await page.$eval(xpath, (ele: any) => ele.datetime)
+        //     console.log("searchTime", text)
+        // } catch (e) {
+        //     console.log('Timed out')
+        // }
+
     }
 
     async verifyTwitterContent(twitterLink: string): Promise<boolean> {
         let response
         try {
+            await this.searchTwitter(twitterLink)
             response = await axios.get(twitterLink)
             // response = await axios.get("https://baidu.com")
             console.log(response)
